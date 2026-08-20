f7b713bf63948577242278d9bfc811c95a4215ad
NastyaaaKlt <m9122767382@gmail.com>
2026-08-20 23:14:32 +0500

    Update соцдем.sql

--- соцдем.sql
+++ соцдем.sql
@@ -1,9 +1,12 @@
 
-#Распределение по полу и возрасту всех пользователей, которым показывалась наша реклама
-SELECT sex.sex, age.age, SUM(vision_test.display) AS count_show,
-    ROUND(SUM(vision_test.display) OVER (PARTITION BY sex.sex) * 100.0 / SUM(vision_test.display) OVER (), 2)  AS percent_too
-FROM vision_test 
-	INNER JOIN age ON vision_test.age = age.id_age
+#Распределение по полу всех пользователей, которым показывалась наша реклама
+SELECT sex.sex, SUM(display) AS count_people_views, ROUND(SUM(display)*100.0/(SELECT SUM(display) FROM vision_test), 2) AS percent
+FROM vision_test
 	INNER JOIN sex ON vision_test.sex = sex.id_sex
-GROUP BY sex.sex, age.age;
+GROUP BY sex.sex;
+
+#Распределение по полу и возрасту
+
+
+
 
