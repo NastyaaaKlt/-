722eb382aa339cc2b4208039ac5708aa814fa7a2
NastyaaaKlt <m9122767382@gmail.com>
2026-08-23 21:10:46 +0500

    Update соцдем.sql

--- соцдем.sql
+++ соцдем.sql
@@ -6,6 +6,18 @@ FROM vision_test
 GROUP BY sex.sex;
 
 #Распределение по полу и возрасту
+SELECT sex, age, number_by_age, number_by_sex, ROUND(number_by_age*100.0/number_by_sex, 2) AS percent
+FROM
+(SELECT sex.sex AS sex, age.age AS age, SUM(display) AS number_by_age,
+	CASE
+    	WHEN sex.sex = 'Женский' THEN (SELECT SUM(display) FROM vision_test WHERE sex = 1)
+        when sex.sex = 'Мужской' THEN (SELECT SUM(display) FROM vision_test WHERE sex = 2)
+        ELSE (SELECT SUM(display) FROM vision_test WHERE sex = 3)
+       END AS number_by_sex
+FROM vision_test
+	INNER JOIN sex ON vision_test.sex = sex.id_sex
+    INNER JOIN age ON vision_test.age = age.id_age
+group by sex.sex, age.age);
 
 
 
